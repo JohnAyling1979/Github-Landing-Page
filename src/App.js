fe35c73b94a769b80de4ca35ca2e8d3ceeeb56b2
John Ayling <john.ayling@digintent.com>
2021-06-10 22:09:22 -0500

    Start adding projects.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import LandingPage from './Containers/LandingPage';
 
 function App() {
   return (
     <div>
+      <LandingPage />
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/Components/ProfileHeader/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './index.css';
+import { Jumbotron } from 'react-bootstrap';
+
+function ProfileHeader() {
+  return (
+    <Jumbotron>
+      <h1>Hi, I'm John</h1>
+      <p>
+        I am a software designer currently working for <a href="https://manifold.group" rel="noopener noreferrer" target="_blank">Manifold</a>, an advisory and product development firm. I'm a full-stack developer with front-end experience in the React framework. For the back-end, I work in mostly PHP with some projects using Node.js. I also use React Native for mobile development.
+      </p>
+    </Jumbotron>
+  );
+}
+
+export default ProfileHeader;
