9b4629152bbcc675b2d1d206d1bc07be6f46e078
John Ayling <john.ayling@digintent.com>
2021-06-12 10:08:07 -0500

    Fix url link.

--- src/Components/ProfileHeader.js
+++ src/Components/ProfileHeader.js
@@ -15,7 +15,7 @@ function ProfileHeader() {
 
       <h1>Hi, I'm John</h1>
       <p>
-        I am a software designer currently working for <a href="https://manifold.group" rel="noopener noreferrer" target="_blank">Manifold</a>, an advisory and incubation firm. I'm also CTO for <a href="https://manifold.group" rel="noopener noreferrer" target="_blank">404 Studios</a>. I'm a full-stack developer with front-end experience in the React framework. For the back-end, I work in mostly PHP with some projects using Node.js. I also use React Native for mobile development.
+        I am a software designer currently working for <a href="https://manifold.group" rel="noopener noreferrer" target="_blank">Manifold</a>, an advisory and incubation firm. I'm also CTO for <a href="https://www.404gamestudios.com" rel="noopener noreferrer" target="_blank">404 Studios</a>. I'm a full-stack developer with front-end experience in the React framework. For the back-end, I work in mostly PHP with some projects using Node.js. I also use React Native for mobile development.
       </p>
     </Jumbotron>
   );
